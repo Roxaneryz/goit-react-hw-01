d944f915237aa23c7a12747b39ddc60e016d4b6a
Roxaneryz <roxane5666@yahoo.gr>
2024-05-19 21:33:08 +0300

    Profile finish

--- src/componets/Profile/Profile.jsx
+++ src/componets/Profile/Profile.jsx
@@ -1,30 +1,47 @@
-const Profile = ({ name, tag, location, image, stats }) => {
-  return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
-      </div>
 
-      <ul>
+
+
+import styles from "./Profile.module.css";
+
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.profileHeader}>
+        <img
+          className={styles.profileImage}
+          src={
+            image || "https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          }
+          alt="User avatar"
+        />
+        <p className={styles.profileDetails}>{name}</p>
+        <p className={styles.profileDetails}>@{tag}</p>
+        <p className={styles.profileDetails}>{location}</p>
+      </div>
+      <ul className={styles.profileStats}>
         <li>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span>{followers}</span>
         </li>
         <li>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span>{views}</span>
         </li>
         <li>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span>{likes}</span>
         </li>
       </ul>
     </div>
   );
 };
+
 
 export default Profile;
 
