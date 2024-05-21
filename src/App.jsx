4cf559869d9c1f79e938800843749775e57ce68c
Roxaneryz <roxane5666@yahoo.gr>
2024-05-21 16:34:44 +0300

    transactions

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Profile from "./componets/Profile/Profile";
 import userData from "../src/data/userData.json";
 import friends from "../src/data/friends.json";
 import FriendList from "./componets/FriendList/FriendList";
-// import transactions from "./data/transactions.json";
+import transactions from "./data/transactions.json";
+import TransactionHistory from "./componets/TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </>
   );
 };
