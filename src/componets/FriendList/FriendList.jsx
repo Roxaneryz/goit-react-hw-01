b6fcfcc1bba2fc082a380d0a779596170c6071b0
Roxaneryz <roxane5666@yahoo.gr>
2024-05-21 18:13:53 +0300

    hwrk react 1

--- src/componets/FriendList/FriendList.jsx
+++ src/componets/FriendList/FriendList.jsx
@@ -1,25 +1,20 @@
-// import style from "./FriendsList.module.css"
-import FriendListItem from "./FriendsListItem.jsx"
-
+import css from "./FriendList.module.css";
+import FriendListItem from "./FriendsListItem.jsx";
 
 const FriendList = ({ friends }) => {
-
-
-    return (
-        <ul className="friend-container">
-          {friends.map((friend) => (
-            <li key={friend.id}>
-              <FriendListItem
-                avatar={friend.avatar}
-                name={friend.name}
-                isOnline={friend.isOnline}
-              />
-            </li>
-          ))}
-        </ul>
-      
-    );
-}
-        
+  return (
+    <ul className={css.friendList}>
+      {friends.map((friend) => (
+        <li className={css.friendItem} key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default FriendList;
--- src/componets/FriendList/FriendsListItem.jsx
+++ src/componets/FriendList/FriendsListItem.jsx
@@ -2,13 +2,21 @@ import css from "./FriendList.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline, }) => {
     
-    return <div className="firend-item">
+    return (
+        <div className={css.friendContainer}>
         <img src={avatar} alt={name} width="48" />
         <p>{name}</p>
-        <p className={[css.status] [isOnline ? "{css.online}" : "css.offline"]}>
-            {isOnline ? "Online" : "Offline"}
+        <p
+          className={[css.status, isOnline ? css.online : css.offline].join(
+            " "
+          )}
+        >
+          {isOnline ? "Online" : "Offline"}
         </p>
-    </div>
+        {/* <p className={[css.status] [isOnline ? "{css.online}" : "css.offline"]}>
+            {isOnline ? "Online" : "Offline"} */}
+      </div>
+    );
 };
 
 
